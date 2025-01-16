58cf0ec06fd2f99b7d5a04fd614ce9214d454e6f
John Yu <byu9@ncsu.edu>
2025-01-16 10:26:19 -0500

    move NYISO dataset to the top level
    add dataset compilation routines for intra-day

--- nyiso_dataset/compile_dataset.py
+++ nyiso_dataset/compile_dataset.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import logging
 from glob import glob
 from pathlib import Path
@@ -27,11 +26,17 @@ def _unpack_zip_file(filename, unpack_folder):
         file.extractall(unpack_folder)
 
 
-def _purge_folder(folder):
+def _purge_folder_of_csv_files(folder):
     _logger.info(f'Purging "{folder}".')
 
-    for item in glob(f'{folder}/*.csv'):
-        Path(item).unlink()
+    for file in glob(f'{folder}/*.csv'):
+        Path(file).unlink()
+
+
+def _unpack_zip_files(filenames, unpack_folder):
+    _purge_folder_of_csv_files(unpack_folder)
+    for filename in filenames:
+        _unpack_zip_file(filename, unpack_folder)
 
 
 def _concat_csv_fragments(frag_folder):
@@ -47,11 +52,11 @@ def _concat_csv_fragments(frag_folder):
     return dataset
 
 
-def _compile_load_dataset(year_month_list):
+def _build_load_dataset(year_month_list):
     fetch_folder = 'fetch_load'
     unpack_folder = 'unpack_load'
 
-    _purge_folder(unpack_folder)
+    _purge_folder_of_csv_files(unpack_folder)
 
     for year, month in year_month_list:
         zip_url = f'http://mis.nyiso.com/public/csv/pal/{year:4}{month:02}01pal_csv.zip'
@@ -69,11 +74,11 @@ def _compile_load_dataset(year_month_list):
     return dataset
 
 
-def _compile_weather_dataset(year_month_list):
+def _build_weather_dataset(year_month_list):
     fetch_folder = 'fetch_weather'
     unpack_folder = 'unpack_weather'
 
-    _purge_folder(unpack_folder)
+    _purge_folder_of_csv_files(unpack_folder)
 
     for year, month in year_month_list:
         zip_url = f'http://mis.nyiso.com/public/csv/lfweather/{year:4}{month:02}01lfweather_csv.zip'
@@ -99,22 +104,14 @@ def _compile_weather_dataset(year_month_list):
     return dataset
 
 
-def _compile_dataset():
-    year_month_list = [
-        (year, month)
-
-        for year in [2022, 2023, 2024]
-        for month in range(1, 13)
-    ]
-
-    # Compile load data, remove unused columns
-    load = _compile_load_dataset(year_month_list).reset_index().set_index(['Time Stamp', 'Name'])
+def _join_datasets(load, weather):
+    # Remove unused load columns
+    load = load.reset_index().set_index(['Time Stamp', 'Name'])
     load.drop(columns=['PTID'], inplace=True)
 
-    # Compile weather data
     # Calculate ambient temperature
     # Join Station ID in weather with Station ID in station-to-zone table
-    weather = _compile_weather_dataset(year_month_list)
+    weather = weather.copy()
     weather['AmbientT'] = (weather['Max Temp'] + weather['Min Temp']) / 2
     weather_mapping = pd.read_csv('weather_station_mapping.csv', index_col='Station ID')
     weather = weather.join(weather_mapping, on='Station ID', how='left')
@@ -129,26 +126,42 @@ def _compile_dataset():
     dataset = load.join(weather, how='left').reset_index().set_index('Time Stamp')
     dataset = pd.pivot(dataset, columns='Name').swaplevel(axis='columns')
 
-    # Resample to 15 minute datasets
-    dataset = dataset.resample('1h').mean().round(2)
+    dataset.index.name = 'Index'
+    return dataset
+
+
+def _save_feature(dataset, save_as):
+    float_format = '%.6f'
+    dataset.drop(columns=['Target']).to_csv(save_as, float_format=float_format)
+
+
+def _save_target(dataset, save_as):
+    float_format = '%.6f'
+    dataset[['Target']].to_csv(save_as, float_format=float_format)
+
+
+def _compile_dataset_for_da(dataset):
+    # Resample to hourly datasets
+    dataset = dataset.resample('1h').mean()
 
     # Rename load column as target
     dataset.rename(columns={'Load': 'Target'}, inplace=True)
+
+    def _add_features(_dataset):
+        _dataset['Hour'] = _dataset.index.hour
+        _dataset['Day'] = _dataset.index.day
+        _dataset['Month'] = _dataset.index.month
+        _dataset['DoW'] = _dataset.index.dayofweek
+        _dataset['Prior24'] = _dataset['Target'].shift(24)
+        _dataset['Prior48'] = _dataset['Target'].shift(48)
+        _dataset['Prior168'] = _dataset['Target'].shift(168)
 
     # Split dataset by zones
     for zone in dataset.columns.get_level_values(0):
         zonal = dataset[zone].copy()
 
         # Construct additional columns
-        zonal.index.name = 'Index'
-        zonal['Hour'] = zonal.index.hour
-        zonal['Day'] = zonal.index.day
-        zonal['Month'] = zonal.index.month
-        zonal['DoW'] = zonal.index.dayofweek
-        zonal['Prior24'] = zonal['Target'].shift(24)
-        zonal['Prior48'] = zonal['Target'].shift(48)
-        zonal['Prior168'] = zonal['Target'].shift(168)
-
+        _add_features(zonal)
         zonal.dropna(axis='index', inplace=True)
 
         # Split into train and test and drop timestamps
@@ -157,12 +170,47 @@ def _compile_dataset():
         zonal_test = zonal[~train_indices]
 
         _logger.info(f'Writing datasets for {zone}.')
-
-        zonal_train.drop(columns='Target').to_csv(f'train/feature-{zone}.csv')
-        zonal_test.drop(columns='Target').to_csv(f'test/feature-{zone}.csv')
-
-        zonal_train[['Target']].to_csv(f'train/target-{zone}.csv')
-        zonal_test[['Target']].to_csv(f'test/target-{zone}.csv')
+        _save_feature(zonal_train, f'da_train/feature-{zone}.csv')
+        _save_feature(zonal_test, f'da_test/feature-{zone}.csv')
+        _save_target(zonal_train, f'da_train/target-{zone}.csv')
+        _save_target(zonal_test, f'da_test/target-{zone}.csv')
 
 
-_compile_dataset()
+def _compile_dataset_for_id(dataset):
+    # Resample to 15 minute datasets
+    dataset = dataset.resample('15min').mean()
+
+    # Rename load column as target
+    dataset.rename(columns={'Load': 'Target'}, inplace=True)
+
+    # Split dataset by zones
+    for zone in dataset.columns.get_level_values(0):
+        zonal = dataset[zone].copy()
+        zonal.dropna(axis='index', inplace=True)
+
+        # Split into train and test and drop timestamps
+        train_indices = zonal.index < '2024-01-01'
+        zonal_train = zonal[train_indices]
+        zonal_test = zonal[~train_indices]
+
+        _logger.info(f'Writing datasets for {zone}.')
+        _save_target(zonal_train, f'id_train/target-{zone}.csv')
+        _save_target(zonal_test, f'id_test/target-{zone}.csv')
+
+
+def _compile_datasets():
+    year_month_list = [
+        (year, month)
+        for year in [2022, 2023, 2024]
+        for month in range(1, 13)
+    ]
+
+    load_dataset = _build_load_dataset(year_month_list)
+    weather_dataset = _build_weather_dataset(year_month_list)
+    dataset = _join_datasets(load_dataset, weather_dataset)
+
+    _compile_dataset_for_da(dataset)
+    _compile_dataset_for_id(dataset)
+
+
+_compile_datasets()
